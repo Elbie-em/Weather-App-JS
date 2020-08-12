a36f111ef986d377f09cc586cbf05299e1e02529
Elbie-em <emoonga1993@gmail.com>
2020-08-13 01:50:36 +0200

    Add units on api call

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import * as Doman from './modules/doman'
+
 const fetchApiKey = () => {
 	const apiKey = '079e5ccf7838205ec9abca89cac417bd';
 	return apiKey;
 }
 
 const fetchUrl = (location,apiKey) => {
-	let webUrl = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`;
+	let webUrl = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=${apiKey}`;
 	return webUrl;
 }
 
@@ -18,4 +20,12 @@ const getWeatherData = async (location) => {
 	return weatherData;
 }
 
+const displayWeatherInfo = async (data) => {
+	let weatherData = await data;
+	Doman.displayData(weatherData);
+}
 
+
+
+let weatherData = getWeatherData('Mexico city');
+displayWeatherInfo(weatherData);
