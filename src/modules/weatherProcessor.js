750bbac2754e579d08d41d2785a5d9a2da938aca
Elbie-em <emoonga1993@gmail.com>
2020-08-15 00:43:51 +0200

    Add weather processor and move required functions

---
+++ src/modules/weatherProcessor.js
@@ -0,0 +1,27 @@
+import * as API from './apiData';
+import * as Doman from './doman';
+
+const getWeatherData = async (location) => {
+	const apiKey = API.fetchApiKey();
+	const webUrl = API.fetchUrl(location, apiKey);
+
+	try {
+		const response = await fetch(webUrl, { mode: 'cors' });
+		const weatherData = await response.json();
+		return weatherData;
+	} catch (error) {
+		console.log('Invalid')
+	}
+
+}
+
+const displayWeatherInfo = async (data) => {
+	try {
+		let weatherData = await data;
+		Doman.displayData(weatherData);
+	} catch (error) {
+		Doman.showError();
+	}
+}
+
+export {getWeatherData,displayWeatherInfo}
