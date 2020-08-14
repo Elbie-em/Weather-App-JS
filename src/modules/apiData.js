020d3df1ec01f1b28e0dc2e480d370a2033718fe
Elbie-em <emoonga1993@gmail.com>
2020-08-15 00:45:24 +0200

    Add api data module and import  required

---
+++ src/modules/apiData.js
@@ -0,0 +1,11 @@
+const fetchApiKey = () => {
+	const apiKey = '079e5ccf7838205ec9abca89cac417bd';
+	return apiKey;
+}
+
+const fetchUrl = (location, apiKey) => {
+	let webUrl = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=${apiKey}`;
+	return webUrl;
+}
+
+export {fetchApiKey,fetchUrl};
