fe70967df7a70dd1e2e32b517dcdd3aa9ec6df20
Elbie-em <emoonga1993@gmail.com>
2020-08-15 00:44:12 +0200

    Add location processor and move required functions

---
+++ src/modules/locationProcessor.js
@@ -0,0 +1,41 @@
+import * as API from './apiData';
+import * as WEP from './weatherProcessor';
+import * as Doman from './doman';
+
+
+const getCurrentPosition = async (lat, lng) => {
+	const apiKey = API.fetchApiKey();
+	let webUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=${apiKey}`;
+	
+	try {
+		const response = await fetch(webUrl, { mode: 'cors' });
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.log('Invalid')
+	}
+}
+
+const success = async (position) => {
+	const latitude = position.coords.latitude;
+	const longitude = position.coords.longitude;
+	const currentLocationWeatherData = await getCurrentPosition(latitude, longitude);
+	let weatherData = await WEP.getWeatherData(currentLocationWeatherData.name);
+	WEP.displayWeatherInfo(weatherData);
+}
+
+const error = () => {
+	Doman.showError();
+}
+
+const onLoad = () => {
+	if (!navigator.geolocation) {
+		alert('Geolocation is not supported by your browser');
+	} else {
+		navigator.geolocation.getCurrentPosition(success, error);
+	}
+}
+
+export {
+    onLoad
+}
