48dafdfb0317ef5b4c819ff92ac1f31f1ed36099
Elbie-em <emoonga1993@gmail.com>
2020-08-15 00:46:23 +0200

    Move functions to belonging modules

--- src/index.js
+++ src/index.js
@@ -1,78 +1,14 @@
-import * as Doman from './modules/doman'
-
-const fetchApiKey = () => {
-	const apiKey = '079e5ccf7838205ec9abca89cac417bd';
-	return apiKey;
-}
-
-const fetchUrl = (location, apiKey) => {
-	let webUrl = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=${apiKey}`;
-	return webUrl;
-}
-
-const getWeatherData = async (location) => {
-	const apiKey = fetchApiKey();
-	const webUrl = fetchUrl(location, apiKey);
-
-	try {
-		const response = await fetch(webUrl, { mode: 'cors' });
-		const weatherData = await response.json();
-		return weatherData;
-	} catch (error) {
-		console.log('Invalid')
-	}
-
-}
-
-const displayWeatherInfo = async (data) => {
-	try {
-		let weatherData = await data;
-		Doman.displayData(weatherData);
-	} catch (error) {
-		Doman.showError();
-	}
-}
+import * as Doman from './modules/doman';
+import * as Loc from './modules/locationProcessor';
+import * as WEP from '../src/modules/weatherProcessor';
 
 const loadInputData = async () => {
 	const city = Doman.getData('search-input')
-	let weatherData = await getWeatherData(city);
-	displayWeatherInfo(weatherData);
+	let weatherData = await WEP.getWeatherData(city);
+	WEP.displayWeatherInfo(weatherData);
 	Doman.changeBg(weatherData.weather[0].main);
 }
 
 Doman.assignButton('btn-search', loadInputData);
 
-const getCurrentPosition = async (lat, lng) => {
-	const apiKey = fetchApiKey();
-	let webUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&APPID=${apiKey}`;
-	
-	try {
-		const response = await fetch(webUrl, { mode: 'cors' });
-		const data = await response.json();
-		return data;
-	} catch (error) {
-		console.log('Invalid')
-	}
-}
-
-const success = async (position) => {
-	const latitude = position.coords.latitude;
-	const longitude = position.coords.longitude;
-	const currentLocationWeatherData = await getCurrentPosition(latitude, longitude);
-	let weatherData = await getWeatherData(currentLocationWeatherData.name);
-	displayWeatherInfo(weatherData);
-}
-
-const error = () => {
-	alert('Unable to retrieve your Location');
-}
-
-const onLoad = () => {
-	if (!navigator.geolocation) {
-		alert('Geolocation is not supported by your browser');
-	} else {
-		navigator.geolocation.getCurrentPosition(success, error);
-	}
-}
-
-onLoad();
+Loc.onLoad();
