43770550f36c50fb4196a7fbe45f97c37f6f5b40
Elbie-em <emoonga1993@gmail.com>
2020-08-15 16:09:16 +0200

    Call loader and reformat code

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import * as Loc from './modules/locationProcessor';
 import * as WEP from '../src/modules/weatherProcessor';
 
 const loadInputData = async () => {
-	const city = Doman.getData('search-input')
+	const city = Doman.getData('search-input');
+	Doman.showLoader();
 	let weatherData = await WEP.getWeatherData(city);
 	WEP.displayWeatherInfo(weatherData);
 	Doman.changeBg(weatherData.weather[0].main);
 }
 
 Doman.assignButton('btn-search', loadInputData);
-
 Loc.onLoad();
+
+
+
