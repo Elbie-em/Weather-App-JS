34674f1bcf47e1305f56c9264d18b0cd91ae55ee
Elbie-em <emoonga1993@gmail.com>
2020-08-15 16:36:26 +0200

    Post build on loader settings

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import * as Loc from './modules/locationProcessor';
 import * as WEP from '../src/modules/weatherProcessor';
 
 const loadInputData = async () => {
-	const city = Doman.getData('search-input');
 	Doman.showLoader();
+	const city = Doman.getData('search-input');
 	let weatherData = await WEP.getWeatherData(city);
 	WEP.displayWeatherInfo(weatherData);
 	Doman.changeBg(weatherData.weather[0].main);
 }
 
 Doman.assignButton('btn-search', loadInputData);
+Doman.showLoader();
 Loc.onLoad();
 
 
