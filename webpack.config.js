7d1d69d4fd92a20c66b47f717ceb4fc4ea0276f1
Elbie-em <emoonga1993@gmail.com>
2020-08-15 13:48:45 +0200

    Configure dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +7,7 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new Dotenv()
+  ],
 };
