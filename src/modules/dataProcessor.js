1dfbb5487fac0c1f4f2eea1d741218ac19894f38
Elbie-em <emoonga1993@gmail.com>
2020-08-16 21:10:01 +0200

    Add data processor module, add respective processing methods

---
+++ src/modules/dataProcessor.js
@@ -0,0 +1,19 @@
+const convertTime = (unixTimeStamp) => {
+	let date = new Date(unixTimeStamp * 1000);
+	let minutes = date.getMinutes();
+	if (minutes < 10){
+		minutes = `0${minutes}`;
+	}
+	let cDate = `${date.getHours()}:${minutes}`;
+	return cDate;
+}
+
+const convertTemp = (temp) => {
+	const unit = (temp * (9/5)) + 32
+	const conversion = Math.round(unit * 10) / 10
+	return conversion
+}
+
+export {
+    convertTime,convertTemp
+}
