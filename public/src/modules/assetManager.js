4e41d8c74d79dc1813a281c4ceb0630ba13fae7a
Elbie-em <emoonga1993@gmail.com>
2020-08-17 21:32:12 +0200

    Add asset manager, import assets

---
+++ public/src/modules/assetManager.js
@@ -0,0 +1,17 @@
+import Rain from '../assets/videos/rain.mp4';
+import Snow from '../assets/videos/snow.mp4';
+import Clear from '../assets/videos/clearsky.mp4';
+import Clouds from '../assets/videos/clouds.mp4';
+import Vars from '../assets/videos/vars.mp4';
+import ErrorImg from '../assets/images/error.gif';
+
+const Assets = {
+    rain: Rain,
+    snow: Snow,
+    clear: Clear,
+    clouds: Clouds,
+    vars:Vars,
+    errorImg:ErrorImg
+};
+
+export default Assets 
